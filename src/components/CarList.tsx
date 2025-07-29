df38c08640d962614e20cfdaf08406087ef15e81
Dan Moffat <danmoffat@gmail.com>
2025-07-29 03:29:12 +0100

    Added consitional rendering of cars list

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,7 +1,22 @@
 import type { FC } from 'react';
+import { useAppSelector } from '../store';
 
 const CarList: FC = () => {
-  return <div className='w-full rounded bg-neutral-800 p-4'>CarList works</div>;
+  const { cars } = useAppSelector(({ cars }) => cars);
+
+  return (
+    <div className='w-full rounded bg-neutral-800 p-4'>
+      {cars.length ? (
+        cars.map(car => (
+          <div>
+            {car.name} £{car.cost}
+          </div>
+        ))
+      ) : (
+        <div>No cars in list</div>
+      )}
+    </div>
+  );
 };
 
 export default CarList;

99feb09a4e51813a4ff85642c704f345361d7a5d
Dan Moffat <danmoffat@gmail.com>
2025-07-29 01:13:44 +0100

    Cleared up som TS

--- src/store/slices/carsSlice.ts
+++ src/store/slices/carsSlice.ts
@@ -1,15 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+type Car = {
+  id: string;
+  name: string;
+  value: number;
+};
+
 type InitState = {
   name: string;
   cost: number | null;
   searchTerm: string;
-  cars: {
-    id: string;
-    name: string;
-    value: number;
-  }[];
+  cars: Car[];
 };
+
 const initialState: InitState = {
   name: '',
   cost: null,

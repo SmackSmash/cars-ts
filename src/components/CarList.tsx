a94e1a97675dc3b9953449a6c077e32f0ee7916d
Dan Moffat <danmoffat@gmail.com>
2025-07-30 01:40:40 +0100

    Added remove car button

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,15 +1,22 @@
 import type { FC } from 'react';
-import { useAppSelector } from '../store';
+import { removeCar, useAppDispatch, useAppSelector } from '../store';
+import Button from './Button';
 
 const CarList: FC = () => {
   const list = useAppSelector(({ cars: { list } }) => list);
+  const dispatch = useAppDispatch();
+
+  const handleClick = (id: string) => {
+    dispatch(removeCar(id));
+  };
 
   return (
     <div className='w-full rounded bg-neutral-800 p-4'>
       {list.length ? (
         list.map(car => (
-          <div>
+          <div className='flex items-center justify-between'>
             {car.name} £{car.cost}
+            <Button onClick={() => handleClick(car.id)}>Remove</Button>
           </div>
         ))
       ) : (

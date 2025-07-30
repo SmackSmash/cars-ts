e3d53700da4a6ad41e5c7bc1b84c2c4bddcecdae
Dan Moffat <danmoffat@gmail.com>
2025-07-30 04:19:32 +0100

    Lovely reduce

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,7 +1,20 @@
 import type { FC } from 'react';
+import { useAppSelector } from '../store';
 
 const CarValue: FC = () => {
-  return <div className='flex w-full justify-end rounded bg-neutral-800 p-4'>CarValue works</div>;
+  const list = useAppSelector(({ cars: { list } }) => list);
+  const searchTerm = useAppSelector(({ cars: { searchTerm } }) => searchTerm);
+
+  return (
+    <div className='flex w-full justify-end rounded bg-neutral-800 p-4'>
+      Total:{' '}
+      {list
+        .filter(car => car.name !== searchTerm)
+        .reduce((acc, car) => {
+          return acc + car.cost;
+        }, 0)}
+    </div>
+  );
 };
 
 export default CarValue;

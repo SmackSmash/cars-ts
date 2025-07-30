ac559e947d8ef0401dbe0e1e667bff73a9f972f1
Dan Moffat <danmoffat@gmail.com>
2025-07-30 02:57:22 +0100

    Added search functionality

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -4,6 +4,7 @@ import Button from './Button';
 
 const CarList: FC = () => {
   const list = useAppSelector(({ cars: { list } }) => list);
+  const searchTerm = useAppSelector(({ cars: { searchTerm } }) => searchTerm);
   const dispatch = useAppDispatch();
 
   const handleClick = (id: string) => {
@@ -13,15 +14,17 @@ const CarList: FC = () => {
   return (
     <div className='flex w-full flex-col gap-2 rounded bg-neutral-800 p-4'>
       {list.length ? (
-        list.map(({ name, cost, id }) => (
-          <div
-            key={id}
-            className='flex items-center justify-between rounded bg-neutral-900 py-2 pr-2 pl-4'
-          >
-            {name} £{cost}
-            <Button onClick={() => handleClick(id)}>Remove</Button>
-          </div>
-        ))
+        list
+          .filter(({ name }) => name.toLowerCase().includes(searchTerm))
+          .map(({ name, cost, id }) => (
+            <div
+              key={id}
+              className='flex items-center justify-between rounded bg-neutral-900 py-2 pr-2 pl-4'
+            >
+              {name} £{cost}
+              <Button onClick={() => handleClick(id)}>Remove</Button>
+            </div>
+          ))
       ) : (
         <div>No cars in list</div>
       )}
